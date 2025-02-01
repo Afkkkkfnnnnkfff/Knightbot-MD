8af5666cfa2c2573c824d221b9e9652754333550
Dark Aditya <adikilovestory11@gmail.com>
2025-02-01 23:29:00 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Adi',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Aditya Advance Bot",
+  botOwner: 'Aditya', // Your name
+  ownerNumber: '918709851207', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
